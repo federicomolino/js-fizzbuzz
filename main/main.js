7fb90018e5f5440c27d06ce489ac052b3054f6bb
federicomolino <federicomolino450@gmail.com>
2024-12-10 21:24:39 +0100

    add: stampo numeri da e verifico se sono sia multipli di 3 e 5

---
+++ main/main.js
@@ -0,0 +1,12 @@
+//ciclo per stamparmi i numeri da 1 a 20
+for (let i = 1; i <= 20; i++) {
+
+    //verifico che i numeri siano multipli di 3 e 5
+   if (i % 3 == 0 && (i % 5 == 0)) {
+        console.log(`${i} FizzBuzz`)
+
+    // stampo il restante dei numeri
+   } else {
+    console.log(i)
+   }
+}
