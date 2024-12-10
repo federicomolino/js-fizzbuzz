d579d6e6f19d669e36e8b7cd8771d13cf56f463e
federicomolino <federicomolino450@gmail.com>
2024-12-10 21:26:44 +0100

    add: verifico se sono solo multipli di 3 e in caso stampo Fizz

--- main/main.js
+++ main/main.js
@@ -3,10 +3,14 @@ for (let i = 1; i <= 20; i++) {
 
     //verifico che i numeri siano multipli di 3 e 5
    if (i % 3 == 0 && (i % 5 == 0)) {
-        console.log(`${i} FizzBuzz`)
+        console.log(`${i} FizzBuzz`);
+
+    // verifico se sono solo multipli di 3
+   } else if (i % 3 == 0) {
+        console.log(`${i} Fizz`);
 
     // stampo il restante dei numeri
-   } else {
-    console.log(i)
+   } else{
+        console.log(i);
    }
 }
