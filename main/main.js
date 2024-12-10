498c71b213f8d6f924b5a8af077a0524430eeb70
federicomolino <federicomolino450@gmail.com>
2024-12-10 21:33:43 +0100

    fix: totale numeri cambiato a 100

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 //ciclo per stamparmi i numeri da 1 a 20
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= 100; i++) {
 
     //verifico che i numeri siano multipli di 3 e 5
    if (i % 3 == 0 && (i % 5 == 0)) {
