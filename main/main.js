78a60a79664368b5d560f30c4cd6cc9bebcad5ae
federicomolino <federicomolino450@gmail.com>
2024-12-12 18:42:00 +0100

    fix: fix generale

--- main/main.js
+++ main/main.js
@@ -2,20 +2,19 @@
 for (let i = 1; i <= 100; i++) {
 
     //verifico che i numeri siano multipli di 3 e 5
-   if (i % 3 == 0 && (i % 5 == 0)) {
+   if (i % 3 == 0 && (i % 5 === 0)) {
         console.log(`FizzBuzz`);
 
     // verifico se sono solo multipli di 3
-   } else if (i % 3 == 0) {
+   } else if (i % 3 === 0) {
         console.log(`Fizz`);
 
     // verifico se sono solo multipli di 5
-   } else if (i % 5 == 0){
+   } else if (i % 5 === 0){
         console.log(`Buzz`);
     }
         // stampo i numeri che non sono multipli di 3, 5 o entrambi
-        else{
-            console.log(i);
-        }
-   
+    else{
+        console.log(i);
+    }
 }
